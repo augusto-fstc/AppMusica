ef4ab6102347d7bfc2ff3025e9542838cf241321
eric <ericaugustobdp@gmail.com>
2023-06-08 20:48:54 -0300

    Botao play funcionando

---
+++ app/src/main/java/com/example/appmusica/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.appmusica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bach);
+
+    }
+    public void executarSom(View view){
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+        }
+    }
+}
